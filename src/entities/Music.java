a8018a6e3b6fff3e9862cf920fe5e386267091af
Gabriel Soares <gabrielsoacc@gmail.com>
2024-05-25 22:15:52 -0300

    Feat: Created MP3_Player Interface

---
+++ src/entities/Music.java
@@ -0,0 +1,58 @@
+package entities;
+
+public class Music {
+
+    private String name;
+    private String artist;
+    private String album;
+    private String genre;
+    private int year;
+
+    public Music(String name, String artist, String album, String genre, int year) {
+        this.name = name;
+        this.artist = artist;
+        this.album = album;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
--- src/interfaces/MP3_Player.java
+++ src/interfaces/MP3_Player.java
@@ -1,7 +1,17 @@
 package interfaces;
 
+import entities.Music;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public interface MP3_Player {
 
-    MusicList musicList
+    List<Music> musicList = new ArrayList<>();
+
+    void play(List<Music> musicList);
+    void stop(List<Music> musicList);
+    void next(List<Music> musicList);
+    void prev(List<Music> musicList);
 
 }
