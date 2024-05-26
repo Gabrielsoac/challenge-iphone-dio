3c174c4c1f3d4a31fbc5d78aa2e42de68107ce25
Gabriel Soares <gabrielsoacc@gmail.com>
2024-05-25 22:06:56 -0300

    feat: Created project for project DIO - To create iphone with interfaces

---
+++ src/entities/iPhone.java
@@ -0,0 +1,12 @@
+package entities;
+
+public abstract class iPhone {
+
+    private String MAC;
+    private String model;
+    private Double RAM;
+    private Double storagement;
+
+    public void turnOn(){}
+    public void turnOff(){}
+}
---
+++ src/interfaces/MP3_Player.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface MP3_Player {
+
+    MusicList musicList
+
+}
